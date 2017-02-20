e77d8534bbabf36f83995228eb18ff33aa523834
jeiao <mingjie1212@gmail.com>
2017-02-20 10:00:26 +0800

    first commit

---
+++ src/main/java/com/github/mingj/MainTest.java
@@ -0,0 +1,12 @@
+package com.github.mingj;
+
+/**
+ * Created by youlook on 17-2-20.
+ */
+public class MainTest {
+
+    public static void main(String[] args){
+        System.out.println("Main Test!");
+    }
+
+}
---
+++ src/main/java/com/github/mingj/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Created by youlook on 17-2-20.
+ */
+package com.github.mingj;
